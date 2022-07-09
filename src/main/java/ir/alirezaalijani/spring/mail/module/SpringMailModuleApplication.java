ba212d34bb4dd8c61d033db4ece35594529311ed
alireza <alirezaalijani.ir@gmail.com>
2022-07-09 16:51:06 +0430

    Initial commit

---
+++ src/main/java/ir/alirezaalijani/spring/mail/module/SpringMailModuleApplication.java
@@ -0,0 +1,13 @@
+package ir.alirezaalijani.spring.mail.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMailModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/alirezaalijani/spring/mail/module/SpringMailModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.alirezaalijani.spring.mail.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
